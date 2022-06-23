965c246d643c5b27ac3ed4fc280b736142e72712
[Arsilsheikh] <[2018ee257@student.uet.edu.pk]>
2022-06-23 17:25:53 +0500

    message queue added

--- main.cpp
+++ main.cpp
@@ -1,39 +1,62 @@
 #include "mbed.h"
 #include "rtos.h"
+
 DigitalOut led1(PB_7);
+// initializing the GPIO asnd configuring it as output.
 DigitalIn button(PC_13, PullDown);
-static unsigned int button_stat=0;
+// initializing the GPIO asnd configuring it as input with pull down configuration
+Queue<int, 1> queue;
+// queue made to hold the message
+static int message = 1;
 Thread thread1;
+// thread 1 created for the button state
 Thread thread2;
- 
-void led1_thread() {
-    while (true) {
-		printf("%u is the button stat\n",button_stat);
-		while(button_stat==1)
-		{
-        led1 = 1;
-		printf("led on\n");
-		}
-		led1=0;
-		printf("Led\n");
+// thread 2 created for the led state on\off
+
+/**
+ * @brief 
+ * 
+ */
+void led1_thread()
+{
+    while (true)
+    {
+        osEvent evt = queue.get();
+        // message getting from queue
+        if (evt.status == osEventMessage)
+        {
+            // if message matches then this loop will operate
+            printf("led on\n");
+            led1 = 1;
+            ThisThread::sleep_for(500);
+            // delay added for the led to be visible to the observer
+            led1 = 0;
+            // led turned off
+        }
+        printf("Led\n");
         ThisThread::sleep_for(1000);
     }
 }
- 
-void button_thread() {
-    while (true) {
-        while(button == 1)
-		{
-			button_stat=1;
-			printf("Button pressed\n");
-		}
-		button_stat=0;
-		printf("Button\n");
+/**
+ * @brief 
+ * 
+ */
+void button_thread()
+{
+    while (true)
+    {
+        if (button == 1)
+        {
+            // if button is presssed then message will be transfered
+            queue.put(&message);
+        }
+        printf("Button\n");
         ThisThread::sleep_for(1000);
     }
 }
- 
-int main() {
+
+int main()
+{
     thread1.start(button_thread);
     thread2.start(led1_thread);
 }
